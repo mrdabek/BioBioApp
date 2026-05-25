82681542a43204e0dd16908cfb1068446f4155e8
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-05-25 17:07:54 +0200

    Initial commit

---
+++ ConsoleApp1-test/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsoleApp1_test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool programRunning = true;
+        // I set bool variable to define if program is runnnign then while loop to check if this condition is true. 
+        
+        while (programRunning)
+        {
+            Console.WriteLine();
+            Console.WriteLine("=== HUVUDMENY ===");
+            Console.WriteLine();
+            Console.WriteLine("Skriv en siffra för att välja funktion.");
+            Console.WriteLine("1 - Typ av billjet - Ungdom, Standard, Pensionär. Ange ålder så väljer vi rätt alternativ.");
+            Console.WriteLine("2 - Gruppbilljet - Ange ålder och antal personer så räknar vi.");
+            Console.WriteLine("3 - ");
+            Console.WriteLine();
+            Console.WriteLine("0 - Avsluta programmet");
+            Console.WriteLine();
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "0":
+                    Console.WriteLine("Programmet avslutas...");
+                    programRunning = false;
+                    break;
+
+                case "1":
+                    Console.WriteLine("Ange din ålder: ");
+                    int age = int.Parse(Console.ReadLine());
+                    if(age < 20)
+                {
+                    Console.WriteLine("Ungdomspris: 80kr");
+                }
+                    else if (age > 64)
+                    {
+                        Console.WriteLine("Pensionärspris: 90kr");
+                    }
+                    // didn't work must work more on that
+                    
+                    // else if (age < 5 && age > 100)
+                    // {
+                    //     Console.WriteLine("Fri entre.");
+                    // }
+                    else
+                    {
+                        Console.WriteLine("Standardpris: 120kr");
+                    }
+                    break;
+                
+                case "2":
+                    Console.WriteLine("Gruppbilljet: Ange antal personer");
+                    programRunning = false;
+                    break;
+
+
+                default:
+                    Console.WriteLine("Felaktig input.");
+                    break;
+            }
+        }    
+    }
+}
